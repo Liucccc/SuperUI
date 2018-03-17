4c82721d389e1d7eccff54ab74ff601f32aadf22
Liucccc <974854469@qq.com>
2018-03-17 17:37:36 +0800

    20180317

---
+++ Hi.Web/Controllers/ArticleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hi.Web.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
