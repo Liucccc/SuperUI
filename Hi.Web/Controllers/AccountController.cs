aebb34c30c95a7adc533e25e82a89a6cf2c220db
Liucccc <974854469@qq.com>
2018-03-22 10:53:54 +0800

    登录功能

---
+++ Hi.Web/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hi.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string userName, string userPwd)
+        {
+            //return Json(new { type = 2, message = "用户名密码错误！" });
+            return Json(new { type = 1, message = "" });
+        }
+    }
+}
